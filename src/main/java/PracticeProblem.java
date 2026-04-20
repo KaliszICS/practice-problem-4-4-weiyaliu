8394839089cc2bfa8e1b620726be33ba736399de
weiyaliu <343445839@tdsb.ca>
2026-04-20 01:26:36 +0000

    yay

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,35 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	
+	public static boolean hasCapital (String word) {
 
+	boolean flag = false;
+	
+	for(int i = 0; i < word.length(); i++) {
+		char ch = word.charAt(i);
+
+		if (Character.isUpperCase(ch)) {
+			flag = true;
+		}
+	}
+	return flag;
+
+	}
+
+	public static boolean isPrime(int num) {
+		boolean flag = true;
+
+		if (num<=1) {
+			flag = false;
+		}
+
+		for (int i=2; i< num; i++) {
+			if(num % i == 0) {
+				flag = false;
+			}
+		}
+
+		return flag;
+	}
 }
